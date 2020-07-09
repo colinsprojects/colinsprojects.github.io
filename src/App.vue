396fc8d0acb5dd7fe7a4d272b277685f8c7c9019
colinsprojects <32842423+colinsprojects@users.noreply.github.com>
2020-07-10 04:00:46 +1000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+//Material Deisign used from https://vuetifyjs.com/en/getting-started/pre-made-layouts/
+<template>
+  <v-app id="inspire">
+    <v-app-bar xs6 sm12 md18 app color="deep-purple lighten-2" dark>
+      <v-spacer></v-spacer>
+
+      <span class="mr-2">Survey</span>
+
+      <v-btn depressed color="deep-purple lighten-2">
+        <span class="mr-2">Change Language</span>
+        <v-icon>mdi-web</v-icon>
+      </v-btn>
+    </v-app-bar>
+<v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-content v-if="show == 0">
+        <v-btn @click="show = 1" v-show="!show">Start Simple Quiz</v-btn>
+
+        <p>This quiz is designed to ask you about how you feel in regarding locations. You will be asked to choose between two different images for each question.</p>
+
+
+        <v-btn @click="show = 2" v-show="!show">Start Complex Quiz</v-btn>
+          <p>This quiz is designed to ask you about how you feel in regarding locations. You will be asked to rate an image for each question.</p>
+      </v-content>
+
+      <v-content v-if="show == 1">
+        <SimpleQuiz></SimpleQuiz>
+      </v-content>
+
+      <v-content v-if="show == 2">
+        <ComplexQuiz></ComplexQuiz>
+      </v-content>
+    </v-row>
+</v-container>
+  </v-app>
+</template>
+
+<script>
+import ComplexQuiz from "./components/ComplexQuiz";
+import SimpleQuiz from "./components/SimpleQuiz";
+//import Test from "./components/Test"
+
+export default {
+  name: "App",
+
+  components: {
+    SimpleQuiz,
+    ComplexQuiz
+    // Test
+  },
+
+  data: function() {
+    return {
+      show: 0
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/SimpleQuiz.vue
@@ -0,0 +1,142 @@
+  //Component for Simple Quiz option
+  //Uses code from Fireship's Firestore tutorial https://www.youtube.com/watch?v=wvRVfyPKOA0&t=274s
+
+  <template>
+  <div class="SimpleQuiz">
+    <div v-for="(question, index) in questions" :key="question.questions">
+      <div v-show="index === questionIndex">
+        <v-container fluid xs="6">
+          <v-row align="center" justify="center">
+            <h1 class="display-1">Question {{questionIndex+1}}: {{question.question}}</h1>
+          </v-row>
+        </v-container>
+
+        <v-container xs="6">
+          <v-row align="center" justify="center">
+            <v-col flex>
+              <v-card max-width="800">
+                <v-img>
+                  <img :src="question.image1URL" />
+                </v-img>
+                <v-card-actions>
+                  <v-radio-group row v-model="radioButton">
+                    <v-radio label="Image 1" color="red" value="1"></v-radio>
+                  </v-radio-group>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+
+            <v-col flex>
+              <v-card max-height="800" max-width="800">
+                <v-img>
+                  <img :src="question.image2URL" />
+                </v-img>
+                <v-card-actions>
+                  <v-radio-group row v-model="radioButton">
+                    <v-radio label="Image 2" color="indigo" value="2"></v-radio>
+                  </v-radio-group>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <v-spacer></v-spacer>
+
+        <v-row align="center" justify="center">
+          <v-radio-group row v-model="radioButton">
+            <v-radio label="I can't decide" color="indigo" value="3"></v-radio>
+          </v-radio-group>
+        </v-row>
+
+        <v-row align="center" justify="center">
+          <v-btn v-if="questionIndex > 0" v-on:click="backButton">Back</v-btn>
+          <v-btn
+            v-if="questionIndex < questions.length-1"
+            v-on:click="nextButton"
+            :disabled="radioButton == null"
+          >Next</v-btn>
+          <div v-if="questionIndex === questions.length-1">
+            <v-btn color="green" v-on:click="submit">submit</v-btn>
+          </div>
+        </v-row>
+      </div>
+
+      <v-footer color="deep-purple lighten-2" app>
+        <v-spacer></v-spacer>
+
+        <span class="white--text">{{ questions.length-questionIndex-1 }} questions remaining</span>
+      </v-footer>
+    </div>
+    <div v-if="questionIndex === questions.length">
+      <ThankYou></ThankYou>
+    </div>
+    {{ answers }}
+  </div>
+</template>
+
+<script>
+import { db } from "@/firebase";
+import firebase from "firebase/app";
+import ThankYou from "./ThankYou";
+
+export default {
+  components: {
+    ThankYou
+  },
+  data: function() {
+    return {
+      questions: [],
+      questionIndex: 0,
+      radioButton: null,
+      answers: []
+    };
+  },
+  firestore: {
+    questions: db.collection("SimpleQuiz")
+  },
+  created() {
+    this.$bind("SimpleQuiz", db.collection("SimpleQuiz")).then(questions => {
+      this.questions === questions;
+    });
+  },
+  methods: {
+    // Go to next question
+    nextButton: function() {
+      this.answers[this.questionIndex] = this.radioButton;
+      this.questionIndex++;
+    },
+    // Go to previous question
+    backButton: function() {
+      this.questionIndex--;
+    },
+    submit: function() {
+      this.answers[this.questionIndex] = this.radioButton;
+      this.questionIndex++;
+
+      const db = firebase.firestore();
+      const increment = firebase.firestore.FieldValue.increment(1);
+
+      var arrayLength = this.answers.length;
+
+
+      //loop through all the answers from the quiz
+      for (var i = 0; i < arrayLength; i++) {
+        var count = i+1;
+        const answersRef = db.collection("SimpleQuiz").doc("" + count);
+        if (this.answers[i] == 1) answersRef.update({ image1Votes: increment });
+        else if (this.answers[i] == 2)
+          answersRef.update({ image2Votes: increment });
+        else if (this.answers[i] == 3)
+          answersRef.update({ neutralVotes: increment });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+img {
+  background-size: cover !important;
+}
+</style>
